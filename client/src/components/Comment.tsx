df0bcba53bf38828437a2585b58c715faa60146f
Hendrix1995 <hgud55@naver.com>
2022-02-22 14:17:35 +0900

    FIX: comment 위치 조절 FIX: post font-size 축소

--- client/src/components/Comment.tsx
+++ client/src/components/Comment.tsx
@@ -3,14 +3,6 @@ import React from "react";
 function Comment() {
     return (
         <section className="comment">
-            <div className="comment-input-container">
-                <input className="comment-input" />
-                <div className="comment-button-container">
-                    <button className="comment-button">댓글 쓰기</button>
-                    <button className="comment-button">댓글 쓰기 + 추천</button>
-                    <button className="comment-button">비추천</button>
-                </div>
-            </div>
             <div className="comment-board-container">
                 <div className="comment-created-container">
                     <div className="comment-created-user-box">
@@ -20,6 +12,14 @@ function Comment() {
                     <div className="comment-created">2022.02.20</div>
                 </div>
                 <div className="comment-content">오...</div>
+            </div>
+            <div className="comment-input-container">
+                <input className="comment-input" />
+                <div className="comment-button-container">
+                    <button className="comment-button">댓글 쓰기</button>
+                    <button className="comment-button">댓글 쓰기 + 추천</button>
+                    <button className="comment-button">비추천</button>
+                </div>
             </div>
         </section>
     );
